974469c8a342d33143aad753f6567728d0c13f56
Matyasmester <matyas.zoboki@gmail.com>
2023-11-17 22:13:52 +0100

    Move only on LMB release event.

--- hero_icon_move.cs
+++ hero_icon_move.cs
@@ -6,6 +6,7 @@ public partial class hero_icon_move : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// (._(._(._.)_.)_.)
 		GD.Print("Hello World!!$$$NAGafasomééé");
 	}
 
@@ -20,6 +21,8 @@ public partial class hero_icon_move : Node
 		// Mouse in viewport coordinates.
 		if (@event is InputEventMouseButton eventMouseButton)
 		{
+			if (eventMouseButton.ButtonIndex != MouseButton.Left || eventMouseButton.IsReleased()) return;
+
 			GD.Print("Mouse Click/Unclick at: ", eventMouseButton.Position);
 			Node2D iconNode = GetNode<Node2D>("HeroIconSprite");
 			// GlobalPos és Pos nem ugyanaz (sima Pos relatív)
